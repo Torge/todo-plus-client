ede8d550dba91ad33c26fec36419eebb8c68487d
Torge Jürgensen <torge.juergensen@codeanker.de>
2017-11-04 16:46:31 +0100

    init vuetify layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    <nuxt/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+    </v-toolbar>
+    <main>
+      <v-content>
+        <v-container fluid>
+          <router-view></router-view>
+        </v-container>
+      </v-content>
+    </main>
+    <v-footer app></v-footer>
+  </v-app>
 </template>
 
 <style>
